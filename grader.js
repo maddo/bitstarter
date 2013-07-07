c5bf85da3bda8aa4246f3754db0985c694139816
Mike Jablecki <mjablecki@gmail.com>
2013-07-07 23:47:30 +0000

    Allow grader to pull from a URL

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var CACHEFILE_DEFAULT = "cache.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,7 +67,20 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to index.html')
         .parse(process.argv);
+    
+    if (program.url) {
+        rest.get(program.url).on('complete',function(result, response) {
+            if (result instanceof Error) {
+                console.error("Unable to cache url");
+            } else {
+                fs.writeFileSync(CACHEFILE_DEFAULT, result)
+                program.file = CACHEFILE_DEFAULT;
+            }
+        });
+    }
+
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
